b603269016d32e1d4fa437e553ac9bd57362ac5e
schwiti6190 <58079399+schwiti6190@users.noreply.github.com>
2021-03-08 14:41:28 +0100

    code improvements and documentation

--- register.lua
+++ register.lua
@@ -1,25 +1,14 @@
 local modName = g_currentModName
 local modDirectory = g_currentModDirectory
 
-local ClickToSwitchRegister = {}
-
-function init()
-	VehicleTypeManager.validateVehicleTypes = Utils.prependedFunction(VehicleTypeManager.validateVehicleTypes, validateVehicleTypes)
-end
-
-
+---Register the spec_clickToSwitch in all drivable vehicle,horses ...
 function validateVehicleTypes(vehicleTypeManager)
-	ClickToSwitchRegister.installSpecializations(vehicleTypeManager, modDirectory, modName)
-end
-
-function ClickToSwitchRegister.installSpecializations(vehicleTypeManager, modDirectory, modName)	
 	for typeName, typeEntry in pairs(vehicleTypeManager:getVehicleTypes()) do
 		if SpecializationUtil.hasSpecialization(Drivable, typeEntry.specializations) then
 			vehicleTypeManager:addSpecialization(typeName, modName .. ".clickToSwitch")	
 		end
     end
-
 end
+VehicleTypeManager.validateVehicleTypes = Utils.prependedFunction(VehicleTypeManager.validateVehicleTypes, validateVehicleTypes)
 
-init()
 
